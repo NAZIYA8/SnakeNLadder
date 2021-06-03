10b64b73ab47617d6a36854f0d68ec8d3d0983b5
NAZIYA8 <syeda.naziya896@gmail.com>
2021-06-03 16:49:33 +0530

    Checked options NoPlay or Ladder or Snake

--- SnakeNLadder.java
+++ SnakeNLadder.java
@@ -1,22 +1,40 @@
 package Snakenladder;
 
+import java.util.List;
 import java.util.Scanner;
 
 public class SnakeNLadder {
 	public static void main(String[] args) {
-		int position=0;
-	System.out.println("Do you want to start the game yes or no?");
-	Scanner scan = new Scanner(System.in);
-	String ans = scan.next();
-	
-	if (ans.equals("yes")) {
-		System.out.println("Game started at position =  " + position);
-	}
-	else {
-		System.out.println("No Worries");
-	}
-	int dice = (int) (Math.random()*6) + 1;
-    System.out.println("Dice  " + dice);
-	
+		int position = 0;
+		System.out.println("do you want to start the game yes or no?");
+		Scanner scan = new Scanner(System.in);
+		String ans = scan.next();
+
+		if (ans.equals("yes")) {
+			System.out.println("Game started at position = " + position);
+		} else {
+			System.out.println("No Worries");
+		}
+		List<Integer> ladders = List.of(2, 4);
+		List<Integer> snakes = List.of(3);
+		int currentPosition = 0;
+
+		int dice = (int) (Math.random() * 6) + 1;
+		System.out.println("dice  " + dice);
+
+		currentPosition += dice;
+		System.out.println("Position: " + currentPosition);
+		if (ladders.contains(currentPosition)) {
+			System.out.println("Ladder");
+			currentPosition += dice;
+			System.out.println("Forward Position: " + currentPosition);
+		} else if (snakes.contains(currentPosition)) {
+			System.out.println("Snake");
+			currentPosition -= dice;
+			System.out.println("Backward Position: " + currentPosition);
+		} else {
+			System.out.println("No Play");
+		}
+
 	}
 }
