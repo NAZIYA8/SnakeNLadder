cdef2066ed4158a6f3c676a3535a52100683f6f6
NAZIYA8 <syeda.naziya896@gmail.com>
2021-06-03 16:06:05 +0530

    Start the game at position 0

--- SnakeNLadder.java
+++ SnakeNLadder.java
@@ -1,7 +1,18 @@
 package Snakenladder;
 
+import java.util.Scanner;
+
 public class SnakeNLadder {
 	public static void main(String[] args) {
-		System.out.println("Welcome to the game Snake and Ladder");
+		int start = 0;
+		System.out.println("Do you want to start the game yes or no?");
+		Scanner scan = new Scanner(System.in);
+		String ans = scan.next();
+
+		if (ans.equals("yes")) {
+			System.out.println("Game started at Position = " + start);
+		} else {
+			System.out.println("No Worries");
+		}
 	}
 }
