d37935a49a5360510409c3eb43123880acaf16c2
NAZIYA8 <syeda.naziya896@gmail.com>
2021-06-03 17:55:24 +0530

    Reporting the dice count and position after every die roll

--- SnakeNLadder.java
+++ SnakeNLadder.java
@@ -6,24 +6,24 @@ import java.util.Scanner;
 public class SnakeNLadder {
 	public static void main(String[] args) {
 		int position = 0;
-		System.out.println("Do you want to start the Game yes or no?");
+		System.out.println("Do you want to start the game yes or no?");
 		Scanner scan = new Scanner(System.in);
 		String ans = scan.next();
 
-		if (ans.equals("y")) {
+		if (ans.equals("yes")) {
 			System.out.println("Game started at position = " + position);
 		} else {
-			System.out.println("No Worries");
+			System.out.println("no worries");
 		}
 		List<Integer> ladders = List.of(2, 4, 9, 33, 52, 80);
 		List<Integer> snakes = List.of(51, 56, 62, 92, 98);
 		int currentPosition = 0;
 		int maxPosition = 100;
+		int iterations = 0;
 		while (currentPosition < maxPosition) {
-
+			iterations++;
 			int dice = (int) (Math.random() * 6) + 1;
 			System.out.println("dice  " + dice);
-			
 			if (currentPosition + dice > 100) {
 				System.out.println("Skipped....");
 				continue;
@@ -45,5 +45,6 @@ public class SnakeNLadder {
 				currentPosition = 0;
 			}
 		}
+		System.out.println("Number of iterations: " + iterations);
 	}
 }
